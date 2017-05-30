3253316ff32be30187ceed341119ad9fc754e502
Addison <ADDISOAX@legal.regn.net>
2017-05-30 17:39:27 +0100

    added a rather bad random bot

---
+++ new house bots/Random/BotExample/BotAIClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+
+
+namespace BotExample
+{
+    internal static class BotAIClass
+    {
+        private static string _opponentName;
+        private static string _lastOpponentsMove;
+        private static int _pointstoWin;
+        private static int _maxRounds;
+        private static int _dynamite;
+        private static Random _random = new Random();
+
+
+        /* Method called when start instruction is received
+         *
+         * POST http://<your_bot_url>/start
+         *
+         */
+        internal static void SetStartValues(string opponentName, int pointstoWin, int maxRounds, int dynamite)
+        {
+            _opponentName = opponentName;
+            _pointstoWin = pointstoWin;
+            _maxRounds = maxRounds;
+            _dynamite = dynamite;
+        }
+
+        /* Method called when move instruction is received instructing opponents move
+         *
+         * POST http://<your_bot_url>/move
+         *
+         */
+        public static void SetLastOpponentsMove(string lastOpponentsMove)
+        {
+            _lastOpponentsMove = lastOpponentsMove;
+        }
+
+
+        /* Method called when move instruction is received requesting your move
+         *
+         * GET http://<your_bot_url>/move
+         *
+         */
+        internal static string GetMove()
+        {
+            return new[] { "ROCK", "PAPER", "SCISSOR", "DYNAMITE", "WATERBOMB" }[_random.Next(4)];
+        }
+    }
+        
+}
